a6f220721924e8a73b7002ba0f16f3fd2afa47e6
antibyteinc <antibyteinc@gamil.com>
2020-05-29 21:29:29 +0300

    Refactoring

--- app/src/main/java/com/example/sked/ActivitySettings.java
+++ app/src/main/java/com/example/sked/ActivitySettings.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class ActivitySettings extends AppCompatActivity {
 
@@ -12,6 +14,14 @@ public class ActivitySettings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        Button btnDog = findViewById(R.id.btn_dog);
+        btnDog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "гав", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         ImageButton btnBack = findViewById(R.id.btn_back);
         btnBack.setOnClickListener(new View.OnClickListener() {
