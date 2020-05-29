889b5534daa3adabe2f928723f6e0c192433aa60
antibyteinc <antibyteinc@gamil.com>
2020-05-29 21:03:23 +0300

    Add logo for loading app

---
+++ app/src/main/java/com/example/sked/SplashScreenActivity.java
@@ -0,0 +1,16 @@
+package com.example.sked;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        startActivity(new Intent(this, MainActivity.class));
+    }
+}
